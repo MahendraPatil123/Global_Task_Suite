865365ae1683aa1cc7bacf5bc05f0fb1d0e25358
Firoz Ahmad <firoz.ahmad@sparsolutions.com>
2024-08-28 01:58:16 +0530

    modified keys

--- Gsuit-Front-End/src/authConfig.js
+++ Gsuit-Front-End/src/authConfig.js
@@ -2,7 +2,7 @@ import { PublicClientApplication } from "@azure/msal-browser";
 
 const msalConfig = {
     auth: {
-        clientId: "46a1ffde-c359-4db3-b36a-a224743b0f7d", 
+        clientId: "e35be651-d860-4ab9-93ac-06e2d774a60c", 
         authority: "48342a33-25ad-4d79-b854-4d66877e41c1",
         redirectUri: "https://agreeable-meadow-094f89710.5.azurestaticapps.net/.auth/login/aad/callback",
       },
